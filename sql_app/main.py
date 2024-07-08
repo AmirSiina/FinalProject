a5048a76df8c8f2803b910955bde8cafabe2b239
AmirSina <sina.a.4334@gmail.com>
2024-07-08 13:58:16 +0330

    initial commit

--- sql_app/main.py
+++ sql_app/main.py
@@ -57,6 +57,10 @@ def update_course(cid: str, course_update: schemas.Course, db: Session = Depends
 
 # ---------------Lecturer-----------------
 
+def abcd():
+    pass
+
+
 @app.post("/CreateLecturer/")
 def create_lecturer(lecturer: schemas.Lecturer, db: Session = Depends(get_db)):
     DataValidation.lid_check(lecturer.lid)
